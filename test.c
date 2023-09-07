c618d1cac9cbd347991ae435f0685d12f5611145
Maks <maksmunait@gmail.com>
2023-09-07 18:35:59 +0200

    07/09 change this commit

--- test.c
+++ test.c
@@ -2,11 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
+#include <stdbool.h>
 
 typedef struct Node {
     int data;            // Data element
     struct Node* next;   // Pointer to the next node
-}Cellule, *Liste;
+}*LinkedList;
 
 
 int sommesCroissantes(int li, int col, int tab[][col]);
@@ -15,13 +16,28 @@ void boo(int untab[],int deuxtab[]);
 
 int LireChaine(char **s);
 
-int countLinked(struct Node* lst);
+int countLinked(LinkedList head);
 
-void push(struct Node** head_ref, int new_data); // push in new head in Linked List
+void push_head(LinkedList* head_ref, int new_data); // push in new head in Linked List
 
-void insertAfter(struct Node* prev_node, int new_data); // insert in Linked List 
+void insertAfter(LinkedList prev_node, int new_data); // insert in Linked List 
 
-void append(struct Node** head_ref, int new_data); // append at the end of Linked List
+void append(LinkedList* head_ref, int new_data); // append at the end of Linked List
+
+void truc(LinkedList lst);
+
+void LastValLinkedList(LinkedList head, int new_val);
+
+void PrintLinkedList(LinkedList head);
+
+void manip(LinkedList *head);
+
+bool inLinkedList(LinkedList head, int value);
+
+void PrintInverseLinkedList(LinkedList head);
+
+int AddTrie(LinkedList *head, int x);
+
 
 int main(void){
 
@@ -34,30 +50,45 @@ int main(void){
     //n = LireChaine(&Chaine);
     //printf("%d\n", n);
 
-    struct Node* head = (struct Node*)malloc(sizeof(struct Node));
-    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-    struct Node* newNode2 = (struct Node*)malloc(sizeof(struct Node));
+    LinkedList head = (struct Node*)malloc(sizeof(struct Node));
+    LinkedList secondNode = (struct Node*)malloc(sizeof(struct Node));
+    LinkedList thirdNode = (struct Node*)malloc(sizeof(struct Node));
+    LinkedList fourth = (struct Node*)malloc(sizeof(struct Node));
+    LinkedList lastNode = (struct Node*)malloc(sizeof(struct Node));
 
-    head -> data = 41;              // Assign data
-    head -> next = newNode;
+    head -> data = 2;
+    head -> next = secondNode;
 
-    newNode -> data = 42;           // Assign data
-    newNode -> next = newNode2;     // Initialize the next pointer
+    secondNode -> data = 4;
+    secondNode -> next = thirdNode;
 
-    newNode2 -> data = 43;
-    newNode2 -> next = NULL;
+    thirdNode -> data = 1;
+    thirdNode -> next = fourth;
 
-    struct Node* current = head;
-    while (current != NULL) {
-        current = current->next;} // Move to the next node
+    fourth -> data = 16;
+    fourth -> next = lastNode;
+
+    lastNode -> data = 9;
+    lastNode -> next = NULL;
+
+    //int count = countLinked(head);
+
+    //printf("Count: %d\n", count);
+
+    //truc(head);
+
+    manip(&head);
+    //append(&head, 70);
+    PrintLinkedList(head);
+    printf("%d\n",countLinked(head));
+    PrintInverseLinkedList(head);
+    if (inLinkedList(head, 2)) {printf("%d in Linked List!\n", 2);}
+
     free(head);
-    free(newNode);
-    free(newNode2);
 
-    int count = countLinked(head);
-    printf("Count: %d\n", count);
     return 0;  
 }
+
 
 void boo(int untab[],int deuxtab[]){
     // le test de QCM;
@@ -66,6 +97,7 @@ void boo(int untab[],int deuxtab[]){
     *untab = *deuxtab;
     *deuxtab = tmp;
 }
+
 
 int sommesCroissantes(int li, int col, int tab[][col]){
     int i, j, sommeCourante = 0, sommePrecedente = 0;
@@ -84,6 +116,7 @@ int sommesCroissantes(int li, int col, int tab[][col]){
     return 1;
 }
 
+
 int LireChaine(char **s){
 
     char seen[1025];
@@ -101,33 +134,32 @@ int LireChaine(char **s){
     return nbchar;
 }
 
-int countLinked(struct Node* lst){
-    struct Node* index = lst;
-    int count = 1;
-    while (index != NULL){
+
+int countLinked(struct Node* head){
+    struct Node* curr = head;
+    int count = 0;
+    while (curr != NULL){
         count++;
-        index = index -> next;
+        curr = curr -> next;
     }
+    free(curr);
     return count;
 }
 
-void push(struct Node** head_ref, int new_data)
+
+void push_head(LinkedList* head_ref, int new_data)
 {
-    /* 1. allocate node */
-    struct Node* new_node = (struct Node*)malloc(sizeof(struct Node));
- 
-    /* 2. put in the data  */
-    new_node -> data = new_data;
- 
-    /* 3. Make next of new node as head */
-    new_node->next = (*head_ref);
- 
-    /* 4. move the head to point to the new node */
-    (*head_ref) = new_node;
+    LinkedList new_node = (struct Node*)malloc(sizeof(struct Node));    /* 1. allocate node */
+
+    new_node -> data = new_data;       /* 2. put in the data  */
+
+    new_node->next = (*head_ref);      /* 3. Make next of new node as head */
+
+    (*head_ref) = new_node;            /* 4. move the head to point to the new node */
 }
 
 
-void insertAfter(struct Node* prev_node, int new_data) //insert in Linked List
+void insertAfter(LinkedList prev_node, int new_data) //insert in Linked List
 {
     /*1. check if the given prev_node is NULL */
     if (prev_node == NULL) {
@@ -149,13 +181,13 @@ void insertAfter(struct Node* prev_node, int new_data) //insert in Linked List
 }
 
 
-void append(struct Node** head_ref, int new_data)  // append at the end of the Linked List
+void append(LinkedList* head_ref, int new_data)  // append at the end of the Linked List
 {
     /* 1. allocate node */
-    struct Node* new_node
-        = (struct Node*)malloc(sizeof(struct Node));
+    LinkedList new_node
+        = (LinkedList)malloc(sizeof(struct Node));
  
-    struct Node* last = *head_ref; /* used in step 5*/
+    LinkedList last = *head_ref; /* used in step 5*/
  
     /* 2. put in the data */
     new_node->data = new_data;
@@ -178,4 +210,85 @@ void append(struct Node** head_ref, int new_data)  // append at the end of the L
     /* 6. Change the next of last node */
     last->next = new_node;
     return;
+}
+
+void truc(LinkedList lst){
+    for(; lst != NULL; lst = lst -> next){
+        lst -> data ++;
+    }
+}
+
+void LastValLinkedList(LinkedList head, int new_val){
+
+    while (head -> next != NULL) {
+        head = head -> next; 
+        }
+    head -> data = new_val;
+    head -> next = NULL;
+}
+
+
+void PrintLinkedList(LinkedList head){
+    while (head != NULL){
+
+        printf("Data: %d, Next: %p\n", head -> data, head -> next);
+        head = head -> next;
+    }
+}
+
+void PrintInverseLinkedList(LinkedList head){
+    if (head != NULL){
+        PrintInverseLinkedList(head -> next);
+        printf("Value from last to first element: %d\n", head -> data);
+    }
+}
+
+void manip(LinkedList *head){
+    LinkedList tmp;
+    int i = 1;
+    if (i == 1) {
+        tmp=(*head)->next->next;    /*1*/ 
+        tmp->next->next=*head;      /*2*/ 
+        *head=(*head)->next;        /*3*/ 
+        (*head)->next=NULL;         /*4*/
+        *head=tmp;                  /*5*/ 
+        tmp=tmp->next;              /*6*/ 
+    }
+}
+
+bool inLinkedList(LinkedList head, int value){
+
+    while (head != NULL){
+        if (head -> data == value){
+            return true;
+        }
+        head = head -> next;
+    }
+    return false;
+}
+
+int AddTrie(LinkedList *head, int x){
+    LinkedList tmp = (struct Node*)malloc(sizeof(struct Node));    
+    LinkedList curr = *head; 
+    if(tmp == NULL) return 0;
+    
+    if (*head == NULL) return *head = tmp;
+
+    if (*head -> data >= x){
+        tmp -> next = *head;
+        *head = tmp;
+    }
+    else{
+        while(curr -> next =! NULL && curr -> next-> data < x){
+            curr = curr -> next;
+        }
+        tmp -> next = curr -> next;
+        curr -> next = tmp;
+    }
+    
+    
+    free(tmp);
+    free(curr);
+
+    return 1;
 }
