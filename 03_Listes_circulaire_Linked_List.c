b66b56572f3e246e4a1b87f137f7444361435a35
Maks <98954111+MunMaks@users.noreply.github.com>
2023-11-19 22:35:44 +0100

    Listes_circulaires.c

---
+++ 03_Listes_circulaire_Linked_List.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node{
+    int val;
+    struct Node* next = NULL; 
+} *LinkedList;
+
+
+
+int main(int argc, char* argv[]){
+    
+    return 0;
+}
